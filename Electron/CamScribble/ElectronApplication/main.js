7623b8b7f37edb4f6d0829b51d300bd7ffc91914
Lenny <lennylennylinuxlinux@gmail.com>
2016-12-24 03:40:38 +0500

    Doing the controls html

--- Electron/CamScribble/ElectronApplication/main.js
+++ Electron/CamScribble/ElectronApplication/main.js
@@ -1,25 +1,69 @@
 const {app, BrowserWindow,ipcMain} = require('electron');
 const CamScribble = require('cam_scribble').CamScribble;
 
-let win
 
-function main() {
-  win = new BrowserWindow({
+
+function openCalibrationWindow(onCloseCallback) {
+  var calibrationWindow = new BrowserWindow({
       width:800,
       height:800,
       titleBarStyle: 'hidden',
       resizable: true
   });
 
-  win.loadURL(`file://${__dirname}/calibrateCamera.html`);
+  calibrationWindow.loadURL(`file://${__dirname}/calibrateCamera.html`);
 
-  win.setMenu(null);
+  calibrationWindow.setMenu(null);
 
-  win.webContents.openDevTools();
+  //calibrationWindow.webContents.openDevTools();
 
-  win.on('closed',() => {
+  calibrationWindow.on('closed',() => {
       win = null;
+      onCloseCallback();
   });
+}
+
+
+function startCamScribble() {
+  console.log('starting cam scr');
+}
+
+
+function checkForConfigurationAndStart() {
+  var fs = require('fs');
+  // If a configuration is found, ask the user.
+  // else open the calibration window.
+  fs.stat('calibration.config',function(err,stat) {
+    if(err == null) {
+      const {dialog} = require('electron');
+      var value = dialog.showMessageBox({
+        type:'question',
+        buttons:['Continue','Recalibrate'],
+        message: 'Continue with existing calibration or recalibrate?',
+        cancelId: -1
+      });
+
+      switch(value) {
+        case -1:
+        case 0:
+          startCamScribble();
+          break;
+        case 1:
+          openCalibrationWindow(function() {
+            startCamScribble();
+          });
+      }
+    } else {
+      openCalibrationWindow(function() {
+        startCamScribble();
+      });
+    }
+  });
+}
+
+
+function main() {
+  checkForConfigurationAndStart();
 }
 
 
@@ -27,6 +71,7 @@ app.on('ready',main);
 
 
 app.on('window-all-closed',() => {
+    console.log('closed');
     if (process.platform !== 'darwin') {
         app.quit()
     }
@@ -39,6 +84,7 @@ app.on('activate', () => {
         main();
     }
 });
+
 
 
 
